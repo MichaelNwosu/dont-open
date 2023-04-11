cbdb1dbd3d21b41ce576d30b316c5b7eea893c12
Michael Nwosu <89435091+MichaelNwosu@users.noreply.github.com>
2023-04-11 13:58:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.A, function () {
+    if (start == 0) {
+        if (input.lightLevel() > 88) {
+            music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+        }
+    }
+})
+let start = 0
+start = 0
